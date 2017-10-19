f8fdaebf02dff935512d1e3999a604a62d32c8c3
jiang.xj <937111472@qq.com>
2017-10-19 11:17:01 +0800

    open change and save

--- BlackSugar/AssignAddrTree/mainwindow.cpp
+++ BlackSugar/AssignAddrTree/mainwindow.cpp
@@ -4,22 +4,52 @@
 #include <QTreeWidget>
 #include <QTreeWidgetItem>
 #include <QDebug>
+#include <QMenu>
+#include <QKeySequence>
+#include <QToolBar>
+#include <QMenuBar>
+#include <QMessageBox>
+#include <QLabel>
+#include <QStatusBar>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
-  QTreeWidget *tree = QtTreeManager::createTreeWidgetFromXmlFile("F:/QtTreeManager/SD41P003.ui");
-  QTreeWidgetItem *node;
-  int newAddress = 0;
-  //tree->invisibleRootItem();
-  for (int i = 0; i < tree->topLevelItemCount(); i++) {
-      node = tree->topLevelItem(i);
-      newAddress = changeAddress(node, newAddress);
-  }
-  QtTreeManager::writeTreeWidgetToXmlFile("Result.ui", tree);
-  tree->show();
+  m_openAction = new QAction(tr("Open"), this);
+  m_openAction->setShortcut(QKeySequence::Open);
+  m_openAction->setStatusTip(tr("Open a file."));
+  connect(m_openAction, SIGNAL(triggered()), this, SLOT(open()));
+
+  QMenu *file = menuBar()->addMenu(tr("&File"));
+  file->addAction(m_openAction);
+
+  m_assignAddr = new QAction(tr("Assign Address"), this);
+  m_assignAddr->setShortcut(QKeySequence("A"));
+  m_assignAddr->setStatusTip(tr("Assign address for the file."));
+  m_assignAddr->setEnabled(false);
+  connect(m_assignAddr, SIGNAL(triggered()), this, SLOT(assign()));
+
+  file->addAction(m_assignAddr);
+
+  m_saveAction = new QAction(tr("Save"), this);
+  m_saveAction->setShortcut(QKeySequence::Save);
+  m_saveAction->setStatusTip(tr("Save the file."));
+  m_saveAction->setEnabled(false);
+  connect(m_saveAction, SIGNAL(triggered()), this, SLOT(save()));
+
+  connect(this, SIGNAL(addrSelected()), this, SLOT(enable()));
+
+  file->addAction(m_saveAction);
+
+  m_msgLabel = new QLabel;
+  m_msgLabel->setMinimumSize(m_msgLabel->sizeHint());
+  m_msgLabel->setAlignment(Qt::AlignHCenter);
+
+  statusBar()->addWidget(m_msgLabel);
+  statusBar()->setStyleSheet(QString("QStatusBar::item{border: 0px}"));
 }
 
 MainWindow::~MainWindow()
@@ -65,4 +95,42 @@ int MainWindow::changeAddress(QTreeWidgetItem *node, int newAddress) {
         }
     }
     return newAddress;
+}
+
+void MainWindow::assign() {
+    QTreeWidgetItem *node;
+    int newAddress = 0;
+    //tree->invisibleRootItem();
+    for (int i = 0; i < m_tree->topLevelItemCount(); i++) {
+        node = m_tree->topLevelItem(i);
+        newAddress = changeAddress(node, newAddress);
+    }
+    m_tree->show();
+}
+
+void MainWindow::open() {
+    QString path = QFileDialog::getOpenFileName(this, tr("Open File"), ".", tr("Files(*.ui)"));
+    if (path.length() == 0) {
+        QMessageBox::information(NULL, tr("Path"), tr("You did not select any files."));
+    } else {
+        QMessageBox::information(NULL, tr("Path"), tr("You selected ") + path);
+        emit MainWindow::addrSelected();
+        m_tree = QtTreeManager::createTreeWidgetFromXmlFile(path);
+        m_tree->show();
+    }
+}
+
+void MainWindow::save() {
+    QString path = QFileDialog::getSaveFileName(this, tr("Save File"), ".", tr("Files(*.ui)"));
+    if (path.length() == 0) {
+        QMessageBox::information(NULL, tr("Path"), tr("You did not select any files."));
+    } else {
+        QMessageBox::information(NULL, tr("Path"), tr("You selected ") + path);
+        QtTreeManager::writeTreeWidgetToXmlFile(path, m_tree);
+    }
+}
+
+void MainWindow::enable() {
+    m_assignAddr->setEnabled(true);
+    m_saveAction->setEnabled(true);
 }
